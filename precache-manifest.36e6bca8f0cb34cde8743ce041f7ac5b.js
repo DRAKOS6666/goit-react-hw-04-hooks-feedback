c6efad1558ed1a1a31e2ef5d213aeac4b2a53c45
DRAKOS <drakostius2@gmail.com>
2020-12-18 23:47:56 +0200

    Updates

---
+++ precache-manifest.36e6bca8f0cb34cde8743ce041f7ac5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8155e04e7a9631cbbe029dd8f6e9c9d6",
+    "url": "/goit-react-hw-04-hooks-feedback/index.html"
+  },
+  {
+    "revision": "af40e68ab2c1ab17d8bc",
+    "url": "/goit-react-hw-04-hooks-feedback/static/css/main.ff67f3b4.chunk.css"
+  },
+  {
+    "revision": "e49d574e9e3db7ff2995",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.8d4ea476.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.8d4ea476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af40e68ab2c1ab17d8bc",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/main.4c5e70f0.chunk.js"
+  },
+  {
+    "revision": "ee7833145e7e54fe2960",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/runtime-main.8a59191b.js"
+  }
+]);
